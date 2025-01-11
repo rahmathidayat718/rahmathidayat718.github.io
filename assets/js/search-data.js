76e0f695a8bb947cc932e7a5c34ff0aa0c26dbbb
rahmathidayat718 <87655146+rahmathidayat718@users.noreply.github.com>
2025-01-11 12:58:18 +0000

    Deploying to gh-pages from @ rahmathidayat718/rahmathidayat718.github.io@e2e04266a839c755d0c3ffe1e005337958b062fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,10 +25,10 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "My works, projects &amp; contributions.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/Portfolio/";
           },
         },{id: "nav-repositories",
           title: "repositories",
@@ -442,8 +442,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-qa-practice-project",
+          title: 'QA-Practice-Project',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
